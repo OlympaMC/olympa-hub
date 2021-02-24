2a9c2d945b175459d33adcb5c1022888a1e79d1c
Tristiisch <tristiisch74@hotmail.com>
2021-02-24 15:22:17 +0100

    add serveur name to scoreboard

--- src/java/fr/olympa/hub/OlympaHub.java
+++ src/java/fr/olympa/hub/OlympaHub.java
@@ -28,6 +28,7 @@ import fr.olympa.api.region.tracking.TrackedRegion;
 import fr.olympa.api.region.tracking.flags.Flag;
 import fr.olympa.api.scoreboard.sign.ScoreboardManager;
 import fr.olympa.api.server.OlympaServer;
+import fr.olympa.api.server.ServerStatus;
 import fr.olympa.core.spigot.OlympaCore;
 import fr.olympa.hub.gui.VanishManager;
 import fr.olympa.hub.minigames.utils.GameType;
@@ -54,7 +55,7 @@ public class OlympaHub extends OlympaAPIPlugin implements Listener {
 
 	public ServerInfosListener serversInfos;
 	public VanishManager vanishManager;
-	
+
 	public Location spawn, lightning;
 
 	public MiniGamesManager games;
@@ -63,72 +64,82 @@ public class OlympaHub extends OlympaAPIPlugin implements Listener {
 	@SuppressWarnings("unchecked")
 	@Override
 	public void onEnable() {
-		instance = this;
-		super.onEnable();
-		
-		OlympaCore.getInstance().setOlympaServer(OlympaServer.LOBBY);
-		AccountProvider.setPlayerProvider(OlympaPlayerHub.class, OlympaPlayerHub::new, "lobby", OlympaPlayerHub.COLUMNS);
-		for (GameType game : GameType.values()) game.initBddStatement();
-		OlympaPermission.registerPermissions(HubPermissions.class);
-
-		spawn = getConfig().getLocation("spawn");
-		lightning = getConfig().getLocation("lightning");
-
-		getServer().getPluginManager().registerEvents(new HubListener(), this);
-
 		try {
-			getServer().getPluginManager().registerEvents(new LaunchPadManager(new File(getDataFolder(), "launchpads.yml")), this);
-		} catch (IOException e) {
+			instance = this;
+			super.onEnable();
+
+			OlympaCore.getInstance().setOlympaServer(OlympaServer.LOBBY);
+			AccountProvider.setPlayerProvider(OlympaPlayerHub.class, OlympaPlayerHub::new, "lobby", OlympaPlayerHub.COLUMNS);
+			for (GameType game : GameType.values())
+				game.initBddStatement();
+			OlympaPermission.registerPermissions(HubPermissions.class);
+
+			spawn = getConfig().getLocation("spawn");
+			lightning = getConfig().getLocation("lightning");
+
+			getServer().getPluginManager().registerEvents(new HubListener(), this);
+
+			try {
+				getServer().getPluginManager().registerEvents(new LaunchPadManager(new File(getDataFolder(), "launchpads.yml")), this);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+
+			new SpawnCommand(this).register();
+			new ServerConfigCommand(this).register();
+
+			OlympaGroup.PLAYER.setRuntimePermission("jumppads.use");
+			OlympaGroup.PLAYER.setRuntimePermission("music.favorites", false);
+
+			OlympaCore.getInstance().registerRedisSub(RedisAccess.INSTANCE.connect(), serversInfos = new ServerInfosListener(getConfig().getConfigurationSection("servers")), RedisChannel.BUNGEE_SEND_SERVERSINFOS.name());
+			//RedisAccess.INSTANCE.disconnect();
+
+			OlympaCore.getInstance().getRegionManager().registerRegion(getConfig().getSerializable("zone", Region.class), "zone", EventPriority.HIGH, new Flag() {
+				@Override
+				public ActionResult leaves(Player p, Set<TrackedRegion> to) {
+					super.leaves(p, to);
+					p.teleport(spawn);
+					return ActionResult.TELEPORT_ELSEWHERE;
+				}
+			}.setMessages(null, "§cNe vous égarez pas !", ChatMessageType.ACTION_BAR));
+
+			CitizensAPI.getTraitFactory().registerTrait(TraitInfo.create(ServerTrait.class).withName("server"));
+			CitizensAPI.getTraitFactory().registerTrait(TraitInfo.create(VoteTrait.class).withName("vote"));
+
+			OlympaAPIPermissions.FLY_COMMAND.setMinGroup(OlympaGroup.MINI_YOUTUBER);
+			OlympaAPIPermissions.GAMEMODE_COMMAND.setMinGroup(OlympaGroup.MINI_YOUTUBER);
+
+			games = new MiniGamesManager(this);
+			vanishManager = new VanishManager();
+
+			scoreboards = new ScoreboardManager<>(this, "§e◆ §6§lOlympa§e ◆");
+			scoreboards.addLines(
+					FixedLine.EMPTY_LINE,
+					new FixedLine<>("§6§lProfil:"),
+					new DynamicLine<>(x -> "§7● " + (x.getOlympaPlayer().getGroup() == null ? "§7Aucun grade" : x.getOlympaPlayer().getGroupNameColored())),
+					new DynamicLine<>(x -> "§7● " + x.getOlympaPlayer().getGroup().getColor() + x.getOlympaPlayer().getName()),
+					FixedLine.EMPTY_LINE,
+					new FixedLine<>("§d§lRadio:"),
+					new TimerLine<>(x -> {
+						JukeBoxDatas datasManager = JukeBox.getInstance().datas;
+						if (datasManager == null)
+							return "§§cnon fonctionnel";
+						PlayerData datas = datasManager.getDatas(x.getOlympaPlayer().getUniqueId());
+						if (datas == null) {
+							sendMessage("§cPas de données lors de la mise à jour du scoreboard musique pour %s.", x.getOlympaPlayer().getName());
+							return "§cpas de données";
+						}
+						String songName = datas.getListeningSongName();
+						return "§7● " + (songName == null ? "§oarrêtée" : WordUtils.wrap(songName, 20, "\n§7", false));
+					}, this, 20),
+					FixedLine.EMPTY_LINE,
+					new FixedLine<>("§a§lServeur:"),
+					new DynamicLine<>(x -> "§7● " + OlympaCore.getInstance().getServerName()));
+			scoreboards.addFooters(FixedLine.EMPTY_LINE, CyclingLine.olympaAnimation());
+		} catch (Exception e) {
+			OlympaCore.getInstance().setStatus(ServerStatus.MAINTENANCE);
 			e.printStackTrace();
 		}
-
-		new SpawnCommand(this).register();
-		new ServerConfigCommand(this).register();
-
-		OlympaGroup.PLAYER.setRuntimePermission("jumppads.use");
-		OlympaGroup.PLAYER.setRuntimePermission("music.favorites", false);
-
-		OlympaCore.getInstance().registerRedisSub(RedisAccess.INSTANCE.connect(), serversInfos = new ServerInfosListener(getConfig().getConfigurationSection("servers")), RedisChannel.BUNGEE_SEND_SERVERSINFOS.name());
-		//RedisAccess.INSTANCE.disconnect();
-
-		OlympaCore.getInstance().getRegionManager().registerRegion(getConfig().getSerializable("zone", Region.class), "zone", EventPriority.HIGH, new Flag() {
-			@Override
-			public ActionResult leaves(Player p, Set<TrackedRegion> to) {
-				super.leaves(p, to);
-				p.teleport(spawn);
-				return ActionResult.TELEPORT_ELSEWHERE;
-			}
-		}.setMessages(null, "§cNe vous égarez pas !", ChatMessageType.ACTION_BAR));
-
-		CitizensAPI.getTraitFactory().registerTrait(TraitInfo.create(ServerTrait.class).withName("server"));
-		CitizensAPI.getTraitFactory().registerTrait(TraitInfo.create(VoteTrait.class).withName("vote"));
-
-		OlympaAPIPermissions.FLY_COMMAND.setMinGroup(OlympaGroup.MINI_YOUTUBER);
-		OlympaAPIPermissions.GAMEMODE_COMMAND.setMinGroup(OlympaGroup.MINI_YOUTUBER);
-
-		games = new MiniGamesManager(this);
-		vanishManager = new VanishManager();
-		
-		scoreboards = new ScoreboardManager<>(this, "§e◆ §6§lOlympa§e ◆");
-		scoreboards.addLines(
-				FixedLine.EMPTY_LINE,
-				new FixedLine<>("§6§lProfil:"),
-				new DynamicLine<>(x -> "§7● " + x.getOlympaPlayer().getGroup().getColor() + x.getOlympaPlayer().getName()),
-				new DynamicLine<>(x -> "§7● " + (x.getOlympaPlayer().getGroup() == null ? "§7Aucun grade" : x.getOlympaPlayer().getGroupNameColored())),
-				FixedLine.EMPTY_LINE,
-				new FixedLine<>("§d§lRadio:"),
-				new TimerLine<>(x -> {
-					JukeBoxDatas datasManager = JukeBox.getInstance().datas;
-					if (datasManager == null) return "§§cnon fonctionnel";
-					PlayerData datas = datasManager.getDatas(x.getOlympaPlayer().getUniqueId());
-					if (datas == null) {
-						sendMessage("§cPas de données lors de la mise à jour du scoreboard musique pour %s.", x.getOlympaPlayer().getName());
-						return "§cpas de données";
-					}
-					String songName = datas.getListeningSongName();
-					return "§7● " + (songName == null ? "§oarrêtée" : WordUtils.wrap(songName, 20, "\n§7", false));
-				}, this, 20));
-		scoreboards.addFooters(FixedLine.EMPTY_LINE, CyclingLine.olympaAnimation());
 	}
 
 	@Override
